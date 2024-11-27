d8123b98fabb244efafb3fb869dfbdc0926b1062
Antonio Marcos <antonio.ms25@aluno.ifsc.edu.br>
2024-11-26 21:25:59 -0300

    Initial Commit

---
+++ EhOTrem.java
@@ -0,0 +1,27 @@
+package EhOTrem;
+
+import robocode.*;
+
+public class EhOTrem extends Robot
+{
+    public void run() {
+        while(true) {
+            ahead(100);
+            turnGunRight(360);
+            back(100);
+            turnGunRight(360);
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+
+    public void onHitByBullet(HitByBulletEvent e) {
+        back(10);
+    }
+
+    public void onHitWall(HitWallEvent e) {
+        back(20);
+    }
+}
