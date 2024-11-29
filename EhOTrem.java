9b0d0870bde057550fabf793653f2fa890b05406
Antonio Marcos <antonio.ms25@aluno.ifsc.edu.br>
2024-11-28 22:17:41 -0300

    movimento inicial

--- EhOTrem.java
+++ EhOTrem.java
@@ -1,29 +1,78 @@
 package EhOTrem;
 
 import robocode.*;
-//bombazaro
-//erik-b26
+import java.util.ArrayList;
 
-public class EhOTrem extends Robot
-{
+public class EhOTrem extends AdvancedRobot {
+    private ArrayList<Wave> enemyWaves = new ArrayList<>();
+    private double previousEnemyEnergy = 100;
+
     public void run() {
-        while(true) {
-            ahead(100);
-            turnGunRight(360);
-            back(100);
-            turnGunRight(360);
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+        while (true) {
+            turnRadarRightRadians(Double.POSITIVE_INFINITY);
         }
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        fire(1);
+        double enemyEnergyDrop = previousEnemyEnergy - e.getEnergy();
+        if (enemyEnergyDrop > 0 && enemyEnergyDrop <= 3) {
+            double bulletVelocity = 20 - 3 * enemyEnergyDrop;
+            Wave wave = new Wave(
+                    getX(),
+                    getY(),
+                    getHeadingRadians() + e.getBearingRadians(),
+                    bulletVelocity,
+                    getTime()
+            );
+            enemyWaves.add(wave);
+        }
+        previousEnemyEnergy = e.getEnergy();
+        doSurfing();
     }
 
-    public void onHitByBullet(HitByBulletEvent e) {
-        back(10);
+    public void doSurfing() {
+        double closestDistance = Double.MAX_VALUE;
+        Wave closestWave = null;
+
+        for (Wave wave : enemyWaves) {
+            double distance = wave.getDistance(getTime()) - distanceToWave(wave);
+
+            if (distance < closestDistance) {
+                closestWave = wave;
+                closestDistance = distance;
+            }
+        }
+
+        if (closestWave != null) {
+            double angleToSurf = Math.PI / 2 + closestWave.angle;
+            setTurnRightRadians(Math.sin(angleToSurf));
+            setAhead(100 * Math.cos(angleToSurf));
+        }
     }
 
-    public void onHitWall(HitWallEvent e) {
-        back(20);
+    public double distanceToWave(Wave wave) {
+        double dx = getX() - wave.startX;
+        double dy = getY() - wave.startY;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
+class Wave {
+    public double startX, startY, velocity;
+    public long startTime;
+    public double angle;
+
+    public Wave(double x, double y, double angle, double velocity, long startTime) {
+        this.startX = x;
+        this.startY = y;
+        this.angle = angle;
+        this.velocity = velocity;
+        this.startTime = startTime;
+    }
+
+    public double getDistance(double time) {
+        return (time - startTime) * velocity;
     }
 }
