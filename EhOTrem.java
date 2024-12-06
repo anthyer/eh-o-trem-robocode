4a9ed305d2687f75b6593b2c0de0a2cdc36e30e0
Antonio Marcos <antonio.ms25@aluno.ifsc.edu.br>
2024-12-05 22:22:07 -0300

    Movimentação inicial concluída

--- EhOTrem.java
+++ EhOTrem.java
@@ -1,78 +1,44 @@
 package EhOTrem;
 
 import robocode.*;
-import java.util.ArrayList;
 
 public class EhOTrem extends AdvancedRobot {
-    private ArrayList<Wave> enemyWaves = new ArrayList<>();
-    private double previousEnemyEnergy = 100;
+    Boolean mode1V1 = false;
 
-    public void run() {
-        setAdjustGunForRobotTurn(true);
-        setAdjustRadarForGunTurn(true);
-        while (true) {
-            turnRadarRightRadians(Double.POSITIVE_INFINITY);
+    public void run(){
+        setTremColor();
+
+        while(true) {
+            for(int directionTurns = 0; directionTurns < 100; directionTurns++) {
+                basicMovement(true);
+            }
+            for(int directionTurns = 100; directionTurns > 0; directionTurns--) {
+                basicMovement(false);
+            }
         }
+    }
+
+    public void basicMovement(boolean right){
+        if (right) {
+            setAhead(100);
+            setTurnRight(100);
+        } else {
+            setAhead(-100);
+            setTurnLeft(100);
+        }
+        setTurnRadarRight(360);
+        setTurnGunRight(360);
+        execute();
     }
 
     public void onScannedRobot(ScannedRobotEvent e) {
-        double enemyEnergyDrop = previousEnemyEnergy - e.getEnergy();
-        if (enemyEnergyDrop > 0 && enemyEnergyDrop <= 3) {
-            double bulletVelocity = 20 - 3 * enemyEnergyDrop;
-            Wave wave = new Wave(
-                    getX(),
-                    getY(),
-                    getHeadingRadians() + e.getBearingRadians(),
-                    bulletVelocity,
-                    getTime()
-            );
-            enemyWaves.add(wave);
-        }
-        previousEnemyEnergy = e.getEnergy();
-        doSurfing();
+        fire(1);
     }
 
-    public void doSurfing() {
-        double closestDistance = Double.MAX_VALUE;
-        Wave closestWave = null;
-
-        for (Wave wave : enemyWaves) {
-            double distance = wave.getDistance(getTime()) - distanceToWave(wave);
-
-            if (distance < closestDistance) {
-                closestWave = wave;
-                closestDistance = distance;
-            }
-        }
-
-        if (closestWave != null) {
-            double angleToSurf = Math.PI / 2 + closestWave.angle;
-            setTurnRightRadians(Math.sin(angleToSurf));
-            setAhead(100 * Math.cos(angleToSurf));
-        }
-    }
-
-    public double distanceToWave(Wave wave) {
-        double dx = getX() - wave.startX;
-        double dy = getY() - wave.startY;
-        return Math.sqrt(dx * dx + dy * dy);
-    }
-}
-
-class Wave {
-    public double startX, startY, velocity;
-    public long startTime;
-    public double angle;
-
-    public Wave(double x, double y, double angle, double velocity, long startTime) {
-        this.startX = x;
-        this.startY = y;
-        this.angle = angle;
-        this.velocity = velocity;
-        this.startTime = startTime;
-    }
-
-    public double getDistance(double time) {
-        return (time - startTime) * velocity;
+    public void setTremColor(){
+        setBodyColor(new java.awt.Color(255, 1, 255));
+        setGunColor(new java.awt.Color(205, 148, 25));
+        setBulletColor(new java.awt.Color(255, 1, 213, 255));
+        setScanColor(new java.awt.Color(205, 148, 25));
     }
 }
