46b34d7dc8f157e8b6fa93f1f5bae699cbe694d7
Pablo Batista Danski <batistadfd@gmail.com>
2024-12-09 19:59:29 -0300

    Movimentação e track dos tanks (ainda há erro a corrigir

--- EhOTrem.java
+++ EhOTrem.java
@@ -1,9 +1,12 @@
 package EhOTrem;
-
+import java.awt.geom.*;
 import robocode.*;
+import java.lang.Math;
 
-public class EhOTrem extends Robot
-{
+public class EhOTrem extends Robot {
+    static Point2D.Double[] enemyPoints = new Point2D.Double[10];
+    int count;
+
     public void run() {
         while(true) {
             ahead(100);
@@ -15,7 +18,15 @@ public class EhOTrem extends Robot
 
     public void onScannedRobot(ScannedRobotEvent e) {
         fire(1);
+        double absBearing = e.getBearingRadians() + getHeadingRadians(); // Método correto para obter o ângulo do robô
+        enemyPoints[count] = new Point2D.Double(getX() + e.getDistance() * Math.cos(absBearing),
+                                                getY() + e.getDistance() * Math.sin(absBearing));
+    
+        if (++count >= 10) {
+            count = 0;
+        }
     }
+    
 
     public void onHitByBullet(HitByBulletEvent e) {
         back(10);
