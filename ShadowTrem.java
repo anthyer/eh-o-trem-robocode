25c663f35d3cad07f6a3a28de06f2fbe06fb7e55
Antonio Marcos <antonio.ms25@aluno.ifsc.edu.br>
2024-12-17 22:23:37 -0300

    Predição de ponto futuro adicionada

--- ShadowTrem.java
+++ ShadowTrem.java
@@ -1,20 +1,24 @@
 package EhOTrem;
 
+
 import java.awt.geom.*;
 import robocode.*;
 import robocode.util.Utils;
 import java.util.ArrayList;
 // Ponto-futuro
 // Por: Antonio e Bernardo
-public class EhOTrem extends AdvancedRobot {
+public class ShadowTrem extends AdvancedRobot {
+
 
     // Variáveis compartilhadas
     private Point2D.Double myLocation;
     private Point2D.Double enemyLocation;
 
+
     private ArrayList<EnemyWave> enemyWaves;
     private ArrayList<Integer> surfDirections;
     private ArrayList<Double> surfAbsBearings;
+
 
     private static final int BINS = 47;
     private static final double[] surfStats = new double[BINS];
@@ -22,19 +26,23 @@ public class EhOTrem extends AdvancedRobot {
     private static final double WALL_STICK = 160;
     private double opponentEnergy = 100.0;
 
+
     public void run() {
         enemyWaves = new ArrayList<>();
         surfDirections = new ArrayList<>();
         surfAbsBearings = new ArrayList<>();
         setTremColor();
 
+
         setAdjustGunForRobotTurn(true);  // Ajustar a arma para virar com o robô
         setAdjustRadarForGunTurn(true); // Ajustar o radar para seguir a arma
+
 
         do {
             turnRadarRightRadians(Double.POSITIVE_INFINITY);  // Rastrear o inimigo indefinidamente
         } while (true);
     }
+
 
     public void setTremColor(){
         setBodyColor(new java.awt.Color(255, 1, 255));
@@ -43,16 +51,21 @@ public class EhOTrem extends AdvancedRobot {
         setScanColor(new java.awt.Color(205, 148, 25));
     }
 
+
     public void onScannedRobot(ScannedRobotEvent e) {
         myLocation = new Point2D.Double(getX(), getY());
+
 
         double lateralVelocity = getVelocity() * Math.sin(e.getBearingRadians());
         double absBearing = e.getBearingRadians() + getHeadingRadians();
 
+
         setTurnRadarRightRadians(Utils.normalRelativeAngle(absBearing - getRadarHeadingRadians()) * 2);
+
 
         surfDirections.add(0, lateralVelocity >= 0 ? 1 : -1);
         surfAbsBearings.add(0, absBearing + Math.PI);
+
 
         double bulletPower = opponentEnergy - e.getEnergy();
         if (bulletPower < 3.01 && bulletPower > 0.09 && surfDirections.size() > 2) {
@@ -64,48 +77,29 @@ public class EhOTrem extends AdvancedRobot {
             ew.directAngle = surfAbsBearings.get(2);
             ew.fireLocation = (Point2D.Double) enemyLocation.clone();
 
+
             enemyWaves.add(ew);
         }
+
 
         opponentEnergy = e.getEnergy();
         enemyLocation = project(myLocation, absBearing, e.getDistance());
 
+
         updateWaves();
         doSurfing();
+
 
         // Disparo de bala
         fireAtEnemy(e);
     }
 
-    private Point2D.Double predictEnemyPosition(ScannedRobotEvent e, long time) {
-        double enemyHeading = e.getHeadingRadians();
-        double enemyVelocity = e.getVelocity();
-        double enemyAcceleration = 0;
-
-        double deltaTime = 0;
-        double predictedX = enemyLocation.x;
-        double predictedY = enemyLocation.y;
-
-        while ((++deltaTime) * bulletVelocity(3.0) < Point2D.distance(myLocation.x, myLocation.y, predictedX, predictedY)) {
-            enemyAcceleration = (enemyVelocity - e.getVelocity()) / deltaTime;
-            enemyVelocity += enemyAcceleration;
-            predictedX += Math.sin(enemyHeading) * enemyVelocity;
-            predictedY += Math.cos(enemyHeading) * enemyVelocity;
-
-            if (predictedX < 18.0 || predictedY < 18.0 || predictedX > getBattleFieldWidth() - 18.0 || predictedY > getBattleFieldHeight() - 18.0) {
-                predictedX = Math.min(Math.max(18.0, predictedX), getBattleFieldWidth() - 18.0);
-                predictedY = Math.min(Math.max(18.0, predictedY), getBattleFieldHeight() - 18.0);
-                break;
-            }
-        }
-
-        return new Point2D.Double(predictedX, predictedY);
-    }
 
     private void updateWaves() {
         for (int i = 0; i < enemyWaves.size(); i++) {
             EnemyWave ew = enemyWaves.get(i);
             ew.distanceTraveled = (getTime() - ew.fireTime) * ew.bulletVelocity;
+
 
             if (ew.distanceTraveled > myLocation.distance(ew.fireLocation) + 50) {
                 enemyWaves.remove(i);
@@ -114,12 +108,15 @@ public class EhOTrem extends AdvancedRobot {
         }
     }
 
+
     private EnemyWave getClosestSurfableWave() {
         double closestDistance = Double.POSITIVE_INFINITY;
         EnemyWave surfWave = null;
 
+
         for (EnemyWave ew : enemyWaves) {
             double distance = myLocation.distance(ew.fireLocation) - ew.distanceTraveled;
+
 
             if (distance > ew.bulletVelocity && distance < closestDistance) {
                 surfWave = ew;
@@ -127,16 +124,21 @@ public class EhOTrem extends AdvancedRobot {
             }
         }
 
+
         return surfWave;
     }
+
 
     private void doSurfing() {
         EnemyWave surfWave = getClosestSurfableWave();
 
+
         if (surfWave == null) return;
+
 
         double dangerLeft = checkDanger(surfWave, -1);
         double dangerRight = checkDanger(surfWave, 1);
+
 
         double goAngle = absoluteBearing(surfWave.fireLocation, myLocation);
         if (dangerLeft < dangerRight) {
@@ -145,13 +147,16 @@ public class EhOTrem extends AdvancedRobot {
             goAngle = wallSmoothing(myLocation, goAngle + (Math.PI / 2), 1);
         }
 
+
         setBackAsFront(this, goAngle);
     }
+
 
     private double checkDanger(EnemyWave surfWave, int direction) {
         int index = getFactorIndex(surfWave, predictPosition(surfWave, direction));
         return surfStats[index];
     }
+
 
     private Point2D.Double predictPosition(EnemyWave surfWave, int direction) {
         Point2D.Double predictedPosition = (Point2D.Double) myLocation.clone();
@@ -159,26 +164,33 @@ public class EhOTrem extends AdvancedRobot {
         double predictedHeading = getHeadingRadians();
         double maxTurning, moveAngle, moveDir;
 
+
         int counter = 0;
         boolean intercepted = false;
+
 
         do {
             moveAngle = wallSmoothing(predictedPosition, absoluteBearing(surfWave.fireLocation, predictedPosition) + (direction * (Math.PI / 2)), direction) - predictedHeading;
             moveDir = 1;
+
 
             if (Math.cos(moveAngle) < 0) {
                 moveAngle += Math.PI;
                 moveDir = -1;
             }
 
+
             moveAngle = Utils.normalRelativeAngle(moveAngle);
             maxTurning = Math.PI / 720d * (40d - 3d * Math.abs(predictedVelocity));
             predictedHeading = Utils.normalRelativeAngle(predictedHeading + limit(-maxTurning, moveAngle, maxTurning));
 
+
             predictedVelocity += (predictedVelocity * moveDir < 0 ? 2 * moveDir : moveDir);
             predictedVelocity = limit(-8, predictedVelocity, 8);
 
+
             predictedPosition = project(predictedPosition, predictedHeading, predictedVelocity);
+
 
             counter++;
             if (predictedPosition.distance(surfWave.fireLocation) < surfWave.distanceTraveled + (counter * surfWave.bulletVelocity) + surfWave.bulletVelocity) {
@@ -186,15 +198,19 @@ public class EhOTrem extends AdvancedRobot {
             }
         } while (!intercepted && counter < 500);
 
+
         return predictedPosition;
     }
+
 
     private int getFactorIndex(EnemyWave ew, Point2D.Double targetLocation) {
         double offsetAngle = absoluteBearing(ew.fireLocation, targetLocation) - ew.directAngle;
         double factor = Utils.normalRelativeAngle(offsetAngle) / maxEscapeAngle(ew.bulletVelocity) * ew.direction;
 
+
         return (int) limit(0, (factor * ((BINS - 1) / 2)) + ((BINS - 1) / 2), BINS - 1);
     }
+
 
     private double wallSmoothing(Point2D.Double botLocation, double angle, int orientation) {
         while (!FIELD_RECT.contains(project(botLocation, angle, WALL_STICK))) {
@@ -203,25 +219,31 @@ public class EhOTrem extends AdvancedRobot {
         return angle;
     }
 
+
     private static Point2D.Double project(Point2D.Double sourceLocation, double angle, double length) {
         return new Point2D.Double(sourceLocation.x + Math.sin(angle) * length, sourceLocation.y + Math.cos(angle) * length);
     }
+
 
     private static double absoluteBearing(Point2D.Double source, Point2D.Double target) {
         return Math.atan2(target.x - source.x, target.y - source.y);
     }
 
+
     private static double limit(double min, double value, double max) {
         return Math.max(min, Math.min(value, max));
     }
+
 
     private static double bulletVelocity(double power) {
         return 20.0 - (3.0 * power);
     }
 
+
     private static double maxEscapeAngle(double velocity) {
         return Math.asin(8.0 / velocity);
     }
+
 
     private static void setBackAsFront(AdvancedRobot robot, double goAngle) {
         double angle = Utils.normalRelativeAngle(goAngle - robot.getHeadingRadians());
@@ -242,19 +264,23 @@ public class EhOTrem extends AdvancedRobot {
         }
     }
 
+
     // Função para disparar contra o inimigo
     private void fireAtEnemy(ScannedRobotEvent e) {
-        double bulletPower = Math.min(3.0, Math.max(1.0, getEnergy() / 10)); // Dispara com base na energia disponível
-        Point2D.Double predictedPosition = predictEnemyPosition(e, getTime());
+        double distance = e.getDistance();
+        double firePower = Math.min(3.0, Math.max(1.0, getEnergy() / 10)); // Dispara com base na energia disponível
 
-        double absBearing = absoluteBearing(myLocation, predictedPosition);
-        double gunTurn = Utils.normalRelativeAngle(absBearing - getGunHeadingRadians());
 
-        setTurnGunRightRadians(gunTurn);
-        if (getGunHeat() == 0 && Math.abs(gunTurn) < Math.PI / 18) { // Dispara se a arma estiver pronta e quase alinhada
-            setFire(bulletPower);
+        if (distance < 100) { // Se o inimigo está muito perto, usa potência máxima
+            firePower = 3.0;
         }
+
+
+        double absBearing = e.getBearingRadians() + getHeadingRadians();
+        setTurnGunRightRadians(Utils.normalRelativeAngle(absBearing - getGunHeadingRadians()));
+        setFire(firePower);
     }
+
 
     class EnemyWave {
         Point2D.Double fireLocation;
